0731b4c9ea519c8821f59d36efd2eec0b5ea56d4
Botabay <tohgl49@gmail.com>
2023-04-04 00:13:07 +0600

    add the Profile component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+import s from './Profile.module.css';
+
+const Profile = ({pageName}) => {
+    return (
+        <div className={s.content}>
+            <div className={s.wallpaper}>{pageName}</div>
+            <div className={s.about_me}>
+                <div className={s.avatar}>
+                    <img src='https://avatars.githubusercontent.com/u/51473977?v=4' alt='avatar'></img>
+                </div>
+                <div className={s.profile_data}>
+                    <p>name</p>
+                    <p>date of birth</p>
+                    <p>city</p>
+                    <p>education</p>
+                    <p>web site</p>
+                </div>
+            </div>
+            <div className={s.posts_block}>
+                <h2>My posts</h2>
+                <div>
+                    <div>
+                        <textarea></textarea>
+                    </div>
+                    <button>send</button>
+                </div>
+                <div>list of posts</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
