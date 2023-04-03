fd456e30ab658d7282764324d753a21d4ccf9e77
Botabay <tohgl49@gmail.com>
2023-04-04 00:19:03 +0600

    add "state"

---
+++ src/state/state.js
@@ -0,0 +1,8 @@
+ const messages = [
+    { id: 1, text: 'Hallo everybody' },
+    { id: 2, text: 'Hey, zuuuup!' },
+    { id: 3, text: 'yo' },
+    { id: 4, text: 'Hey guys!' },
+]
+
+export default messages;
