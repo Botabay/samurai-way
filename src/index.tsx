311ee0c6c1491b1a44172033732a71ba3433410f
Botabay <tohgl49@gmail.com>
2023-04-04 00:17:12 +0600

    some  changes for moving js to ts in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//     <App />,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import messages from './state/state'
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App messages={messages}/>
+    </BrowserRouter>
+  </React.StrictMode>
 );
