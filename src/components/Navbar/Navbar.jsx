0cda9706e86689d1f04b6145a2acbed282e9ad6d
Botabay <tohgl49@gmail.com>
2023-04-04 00:12:11 +0600

    add the Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import s from './Navbar.module.css';
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div className={s.navbar}>
+            <div>
+                <div>
+                    <Link to="/">profile</Link>
+                </div>
+                <div>
+                    <Link to="/messages">messages</Link>
+                </div>
+                <div>
+                    <Link to="/news">news</Link>
+                </div>
+                <div>
+                    <Link to="/music">music</Link>
+                </div>
+            </div>
+            <div>
+                <Link to="/settings">settings</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
