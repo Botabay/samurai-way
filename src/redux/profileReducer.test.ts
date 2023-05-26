e85c8b07d4849c1ec167d62006a41c60f9c1e45b
Botabay <tohgl49@gmail.com>
2023-05-26 23:40:21 +0600

    jest-test to profileReducer is done

--- src/redux/profileReducer.test.ts
+++ src/redux/profileReducer.test.ts
@@ -1,15 +1,14 @@
 import { addNewPostAC, updateNewPostTextAC, ProfileDataType, profileReducer } from './profileReducer';
 
-
 test('correct task should be added to correct array', () => {
     const startState: ProfileDataType = {
         posts: [
-          { id: 1, text: 'post1' },
-          { id: 2, text: 'post2' },
-          { id: 3, text: 'post3' }
+            { id: 1, text: 'post1' },
+            { id: 2, text: 'post2' },
+            { id: 3, text: 'post3' }
         ],
         newPostText: 'this is a place for your post'
-      };
+    };
 
     const action = addNewPostAC();
 
@@ -17,12 +16,35 @@ test('correct task should be added to correct array', () => {
 
     expect(endState).toEqual({
         posts: [
-          { id: 1, text: 'post1' },
-          { id: 2, text: 'post2' },
-          { id: 3, text: 'post3' },
-          { id: 4, text: 'this is a place for your post' }
+            { id: 1, text: 'post1' },
+            { id: 2, text: 'post2' },
+            { id: 3, text: 'post3' },
+            { id: 4, text: 'this is a place for your post' }
         ],
         newPostText: ''
-      });
+    });
+
+    expect(endState.newPostText).toBe('')
+    expect(endState.posts.length).toBe(4)
+    expect(endState.posts[3].id).toBeDefined()
+    expect(endState.posts[3].text).toBe('this is a place for your post')
 
 });
+
+test('status of specified NewPostText should be changed', () => {
+    const startState: ProfileDataType = {
+        posts: [
+            { id: 1, text: 'post1' },
+            { id: 2, text: 'post2' },
+            { id: 3, text: 'post3' }
+        ],
+        newPostText: 'this is a place for your post'
+    };
+
+    const action = updateNewPostTextAC('test value')
+
+    const endState = profileReducer(startState, action)
+
+    expect(endState.posts.length).toBe(3)
+    expect(endState.newPostText).toBe('test value')
+})
