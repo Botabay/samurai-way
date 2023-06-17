2aa243f9edb960769f3058c734dc861f5856b956
Botabay <tohgl49@gmail.com>
2023-06-17 23:58:28 +0600

    lesson49 is done

--- src/components/content/Users/Users.tsx
+++ src/components/content/Users/Users.tsx
@@ -1,14 +1,32 @@
 import { UserType, UsersDataType } from "../../../redux/usersReducer"
 import { User } from "./User/User"
 
-type PropsType={
+type PropsType = {
     users: UsersDataType
+    toFollow: any
+    toUnfollow: any
 }
-export const Users=({users}:PropsType)=>{
-    console.log(users);
-    
-   return (<div>{
-        users.map((el:UserType)=><User/>)
+export const Users = ({ users, toFollow, toUnfollow }: PropsType) => {
+    console.log(toFollow);
+
+    return (<div>{
+        users.map((el: UserType) => (
+            <div key={el.id}>
+                <div className="avatarSection">
+                    <p>{el.avatarUrl}</p>
+                    <p>{el.follow ?
+                        <button onClick={() => toUnfollow(el.id)}>unfollow</button> :
+                        <button onClick={() => toFollow(el.id)}>follow</button>}
+                    </p>
+                </div>
+                <div className="infoSection">
+                    <span>{el.fullName}</span>
+                    <span>{el.status}</span>
+                    <span>{el.location.city}</span>
+                    <span>{el.location.country}</span>
+                </div>
+            </div>
+        ))
     }
     </div>)
 }
